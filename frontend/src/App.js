da6f972510f962b17679120344dfb00947fb7556
enzodn <enzodn@protonmail.com>
2020-10-20 16:14:08 +0200

    Header Footer Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Route} from 'react-router-dom'
+import {Container} from 'react-bootstrap'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
-  return (
-    <>
-      <h1>Welcome</h1>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Header/>
+            <main className='py-3'>
+                <Container>
+                    <Route path='/' component={HomeScreen} exact />
+                    <Route path='/product/:id' component={ProductScreen} />
+                </Container>
+            </main>
+            <Footer/>
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ frontend/src/components/Product.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import {Card} from "react-bootstrap";
+import Rating from "./Rating";
+
+const Product = ({product}) => {
+    return (
+        // my-# Responsive top and bottom margin auto (vertical) classes. * can be sm, md, lg or xl. # can be a number between 0 and 5
+        // py-# Responsive padding classes. * can be sm, md, lg or xl. # can be a number between 0 and 5
+        <Card className='my-3 p-3 rounded'>
+            <Link to={` /product/${product._id}`}>
+                <Card.Img src={product.image} variant='top'/>
+            </Link>
+
+            <Card.Body>
+                <Link to={` /product/${product._id}`}>
+                    <Card.Title as='div'>
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </Link>
+
+                <Card.Text as='div'>
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+                </Card.Text>
+
+                <Card.Text as='h3'>${product.price}</Card.Text>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default Product;
