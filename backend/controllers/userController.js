2d9d5581a86d2d6a4acdc321d427ddd0e883fe6c
enzodn <enzodn@protonmail.com>
2020-10-29 04:54:12 +0100

    Login, Register, Update Profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,6 +44,31 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+        const updatedUser = await user.save()
+        res.send({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
 
 // @desc    Register new user
 // @route   POST /api/users/login
@@ -76,4 +101,4 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 })
 
-export {authUser, getUserProfile, registerUser}
+export {authUser, getUserProfile, registerUser, updateUserProfile}
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import {LinkContainer} from 'react-router-bootstrap'
-import {Container, Nav, Navbar} from "react-bootstrap";
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {useDispatch, useSelector} from "react-redux";
+import {logout} from "../actions/userActions";
 
 const Header = () => {
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
+
     return (
         <header>
             <Navbar bg="primary" variant='dark' expand="lg" collapseOnSelect>
@@ -18,12 +29,20 @@ const Header = () => {
                                     <i className='fas fa-shopping-cart'/> Cart
                                 </Nav.Link>
                             </LinkContainer>
-                            <LinkContainer to='/login'>
-                                <Nav.Link>
-                                    <i className='fas fa-user'/> Login
-                                </Nav.Link>
-                            </LinkContainer>
-
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to='/login'>
+                                    <Nav.Link>
+                                        <i className='fas fa-user'/> Login
+                                    </Nav.Link>
+                                </LinkContainer>
+                            )}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
